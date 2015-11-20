bfc0d53d5a03a576659a9287625555bee4841c15
郑煜宇 <yzheng@atfacg.com>
2015-11-20 10:45:41 +0800

    调整补全插件识别生效区域地机制。

--- state.py
+++ state.py
@@ -1,26 +1,28 @@
 import sublime, sublime_plugin, re
 
-class StateAutoComplete(sublime_plugin.EventListener):
+class StateHandler(sublime_plugin.EventListener):
     def on_query_completions(self, view, prefix, locations):
-        if 1 < len(locations):
+        if 1 < len(locations) or not view.score_selector(locations[0], 'meta.action.state'):
             return ([], sublime.INHIBIT_WORD_COMPLETIONS)
-        match = re.match('(选择|(?:最[大小]|如果|当|对比|设置|增加)数据（)(.*)', prefix)
-        if not match:
-            return ([], sublime.INHIBIT_WORD_COMPLETIONS)
-        command = match.group(1)
-        prefix2 = match.group(2)
-        registry = {}
+        pos = prefix.find('（')
         options = []
         options2 = []
-        for region in view.find_by_selector('variable.other.bigine'):
-            name = view.substr(region)
-            pos = name.find(prefix2)
-            if -1 == pos or name in registry:
-                continue
-            registry[name] = 1
-            option = (command + "\t" + name, command + name)
-            if pos:
-                options2.append(option)
-            else:
-                options.append(option)
+        if 0 < pos:
+            pos += 1
+            command = prefix[0:pos]
+            prefix2 = prefix[pos:]
+            registry = {}
+            for region in view.find_by_selector('variable.other.bigine'):
+                name = view.substr(region)
+                if name in registry:
+                    continue
+                registry[name] = 1
+                pos = name.find(prefix2)
+                if -1 == pos:
+                    continue
+                option = (prefix + "\t" + name, command + name)
+                if pos:
+                    options2.append(option)
+                else:
+                    options.append(option)
         return (options + options2, sublime.INHIBIT_WORD_COMPLETIONS)
--- theme.py
+++ theme.py
@@ -2,27 +2,25 @@ import sublime, sublime_plugin
 
 class ThemeAutoComplete(sublime_plugin.EventListener):
     def on_query_completions(self, view, prefix, locations):
-        if 1 < len(locations):
+        if 1 < len(locations) or not view.score_selector(locations[0], 'meta.theme.bigine'):
             return ([], sublime.INHIBIT_WORD_COMPLETIONS)
-        command = '主题：'
-        pos = prefix.find(command)
-        if pos:
-            return ([], sublime.INHIBIT_WORD_COMPLETIONS)
-        prefix2 = prefix[3:]
-        themes = {
-            'dahao': '默认主题',
-            'fl9t': '凤唳九天',
-            '100': '高考恋爱100天'
-        }
         options = []
         options2 = []
-        for id in themes:
-            pos = id.find(prefix2)
-            if -1 == pos:
-                continue
-            option = (command + "\t" + themes[id], command + id)
-            if pos:
-                options2.append(option)
-            else:
-                options.append(option)
+        command = '主题：'
+        if prefix[0:3] == command:
+            themes = {
+                'dahao': '默认主题',
+                'fl9t': '凤唳九天',
+                '100': '高考恋爱100天'
+            }
+            prefix2 = prefix[3:]
+            for id in themes:
+                pos = id.find(prefix2)
+                if -1 == pos:
+                    continue
+                option = (prefix + "\t" + themes[id], command + id)
+                if pos:
+                    options2.append(option)
+                else:
+                    options.append(option)
         return (options + options2, sublime.INHIBIT_WORD_COMPLETIONS)

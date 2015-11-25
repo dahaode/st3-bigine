44aefb6b9f99f0af977c0b14e6f0ec3c8e8ac573
郑煜宇 <yzheng@atfacg.com>
2015-11-25 16:31:54 +0800

    修正作为整合包使用时无法打包地问题。

--- bigine_publish.py
+++ bigine_publish.py
@@ -11,19 +11,17 @@ class BiginePublishCommand(sublime_plugin.WindowCommand):
             if view.is_dirty():
                 sublime.error_message('自动保存失败 ₍₍ (̨̡ ‾᷄ᗣ‾᷅ )̧̢ ₎₎')
                 return
+        try:
+            tpl = sublime.load_resource(os.path.dirname(view.settings().get('syntax')) + '/bootstrap.html')
+        except:
+            sublime.error_message('此功能仅用于大好网站 Bigine 作品发布…\n₍₍ (̨̡ ‾᷄ᗣ‾᷅ )̧̢ ₎₎')
+            return
         src_file = view.file_name()
         if not src_file:
             sublime.error_message('空作品无法发布 ₍₍ (̨̡ ‾᷄ᗣ‾᷅ )̧̢ ₎₎')
             return
-        tpl_file = os.path.dirname(sublime.packages_path()) + '/' + os.path.dirname(view.settings().get('syntax')) + '/bootstrap.html'
-        if not os.path.isfile(tpl_file):
-            sublime.error_message('此功能仅用于大好网站 Bigine 作品发布…\n₍₍ (̨̡ ‾᷄ᗣ‾᷅ )̧̢ ₎₎')
-            return
         fh = open(src_file, encoding='utf-8')
         src = fh.read(os.path.getsize(src_file))
-        fh.close()
-        fh = open(tpl_file)
-        tpl = fh.read(os.path.getsize(tpl_file))
         fh.close()
         html_file = tempfile.gettempdir() + '/bigine.draft.html'
         fh = open(html_file, 'w+', encoding='utf-8')

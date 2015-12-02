072d1dc99b5ab1007336042acd21897eabaeb253
郑煜宇 <yzheng@atfacg.com>
2015-12-02 10:13:15 +0800

    调整调试发布功能对 Bigine 源代码不可用。

--- bigine_publish.py
+++ bigine_publish.py
@@ -2,6 +2,8 @@ import sublime, sublime_plugin, os, tempfile, webbrowser
 
 class BiginePublishCommand(sublime_plugin.WindowCommand):
     def run(self):
+        if not self._ok():
+            return
         view = self.window.active_view()
         if len(view.find_by_selector('invalid.illegal.bigine') or view.get_regions('bigine.error.theme') or view.get_regions('bigine.error.suite')):
             sublime.error_message('源代码中存在错误，请处理 ₍₍ (̨̡ ‾᷄ᗣ‾᷅ )̧̢ ₎₎')
@@ -11,11 +13,6 @@ class BiginePublishCommand(sublime_plugin.WindowCommand):
             if view.is_dirty():
                 sublime.error_message('自动保存失败 ₍₍ (̨̡ ‾᷄ᗣ‾᷅ )̧̢ ₎₎')
                 return
-        try:
-            tpl = sublime.load_resource(os.path.dirname(view.settings().get('syntax')) + '/Resources/bootstrap.html')
-        except:
-            sublime.error_message('此功能仅用于大好网站 Bigine 作品发布…\n₍₍ (̨̡ ‾᷄ᗣ‾᷅ )̧̢ ₎₎')
-            return
         src_file = view.file_name()
         if not src_file:
             sublime.error_message('空作品无法发布 ₍₍ (̨̡ ‾᷄ᗣ‾᷅ )̧̢ ₎₎')
@@ -23,8 +20,15 @@ class BiginePublishCommand(sublime_plugin.WindowCommand):
         fh = open(src_file, encoding='utf-8')
         src = fh.read(os.path.getsize(src_file))
         fh.close()
+        tpl = sublime.load_resource(os.path.dirname(view.settings().get('syntax')) + '/Resources/bootstrap.html')
         html_file = tempfile.gettempdir() + '/bigine.draft.html'
         fh = open(html_file, 'w+', encoding='utf-8')
         fh.write(tpl % src)
         fh.close()
         webbrowser.open_new_tab('file://' + html_file)
+
+    def _ok(self):
+        return 0 < self.window.active_view().score_selector(0, 'text.bigine')
+
+    def is_enabled(self):
+        return self._ok()

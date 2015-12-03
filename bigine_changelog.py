44743b1456e6598c3d1fa2006ba6a2470046edf5
郑煜宇 <yzheng@atfacg.com>
2015-12-03 15:55:07 +0800

    确定版本 0.1.0 。

--- bigine_changelog.py
+++ bigine_changelog.py
@@ -3,10 +3,10 @@ import sublime, sublime_plugin
 class BigineChangelogCommand(sublime_plugin.WindowCommand):
     def run(self):
         view = self.window.new_file()
-        view.set_syntax_file('Packages/Markdown/Markdown.tmLanguage')
         view.set_name('CHANGELOG')
         view.run_command('insert', {
             'characters': sublime.load_resource('Packages/Bigine/Resources/CHANGELOG.md')
         })
+        view.set_syntax_file('Packages/Markdown/Markdown.tmLanguage')
         view.set_read_only(True)
         view.set_scratch(True)

8bd3730a3e667188f0fd2ecae6b20e0e7692cb22
郑煜宇 <yzheng@atfacg.com>
2015-12-03 15:46:00 +0800

    实现最大数据、最小数据、数据合值和数据差值动作块中地数据自动补全功能。

--- state.py
+++ state.py
@@ -6,7 +6,7 @@ class StateHandler(sublime_plugin.EventListener):
             return []
         if 1 < len(locations) or not view.score_selector(locations[0], 'meta.bigine.ref.state'):
             return ([], sublime.INHIBIT_WORD_COMPLETIONS)
-        pos1 = pos2 = 0
+        pos1 = pos2 = pos3 = 0
         if view.score_selector(locations[0], 'meta.bigine.ref.state.bracket'):
             pos1 = prefix.rfind('（')
         if view.score_selector(locations[0], 'meta.bigine.ref.state.comma'):
@@ -18,18 +18,27 @@ class StateHandler(sublime_plugin.EventListener):
             pos += 1
             command = prefix[0:pos]
             prefix2 = prefix[pos:]
-            registry = {}
-            for region in view.find_by_selector('variable.other.state.bigine'):
-                name = view.substr(region)
-                if name in registry:
-                    continue
-                registry[name] = 1
-                pos = name.find(prefix2)
-                if -1 == pos:
-                    continue
+            perline = False
+        elif view.score_selector(locations[0], 'meta.bigine.ref.state.perline'):
+            prefix2 = prefix
+            perline = True
+        else:
+            return ([], sublime.INHIBIT_WORD_COMPLETIONS)
+        registry = {}
+        for region in view.find_by_selector('variable.other.state.bigine'):
+            name = view.substr(region)
+            if name in registry:
+                continue
+            registry[name] = 1
+            pos = name.find(prefix2)
+            if -1 == pos:
+                continue
+            if perline:
+                option = (name, name)
+            else:
                 option = (prefix + "\t" + name, command + name)
-                if pos:
-                    options2.append(option)
-                else:
-                    options.append(option)
+            if pos:
+                options2.append(option)
+            else:
+                options.append(option)
         return (options + options2, sublime.INHIBIT_WORD_COMPLETIONS)

182cea0528e0826abc61c2a11300547030cf66ca
郑煜宇 <yzheng@atfacg.com>
2015-12-03 16:25:02 +0800

    修正本更新日志说明缩进混乱地问题。

--- bigine_changelog.py
+++ bigine_changelog.py
@@ -3,6 +3,8 @@ import sublime, sublime_plugin
 class BigineChangelogCommand(sublime_plugin.WindowCommand):
     def run(self):
         view = self.window.new_file()
+        settings = view.settings()
+        settings.set('auto_indent', False)
         view.set_name('CHANGELOG')
         view.run_command('insert', {
             'characters': sublime.load_resource('Packages/Bigine/Resources/CHANGELOG.md')

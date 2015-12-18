2897d13488ce7041f71469f1c3f965278870cdad
郑煜宇 <yzheng@atfacg.com>
2015-12-18 15:38:06 +0800

    开放语法的在线手册查看功能。

--- bigine_inspect.py
+++ bigine_inspect.py
@@ -1,14 +1,97 @@
-import sublime, sublime_plugin
+import sublime, sublime_plugin, webbrowser
 
 class BigineInspectCommand(sublime_plugin.TextCommand):
     _point = None
     _clob = None
     _type = ''
+    _actions = {
+        '音乐': 'yinyue',
+        '音源': 'yinyue',
+        '画面': 'texie',
+        '特写': 'texie',
+        '人物': 'renwu',
+        '头像': 'renwu',
+        '姿态': 'renwu',
+        '地图': 'ditu',
+        '底图': 'ditu',
+        '交互点': 'ditu',
+        '高亮': 'ditu',
+        '区域': 'ditu',
+        '对应房间': 'ditu',
+        '房间': 'fangjian',
+        '使用地图': 'fangjian',
+        '时刻': 'fangjian',
+        '音效': 'yinxiao',
+        '天气': 'yinxiao',
+        '自动播放': 'zidongbofang',
+        '主角': 'zhujue',
+        '素材包': 'sucaibao',
+        '主题': 'zhuti',
+        '事件': 'shijian',
+        '类型': 'leixing',
+        '条件': 'tiaojian',
+        '内容': 'neirong',
+        '人物出场': 'renwuchuchang',
+        '人物离场' : 'renwulichang',
+        '设置人物' : 'shezhirenwu',
+        '改变神态' : 'gaibianshentai',
+        '人物移动' : 'renwuyidong',
+        '独白' : 'dubai',
+        '对白' : 'duibai',
+        '提示' : 'tishi',
+        '旁白' : 'pangbai',
+        '自动存档' : 'zidongcundang',
+        '游戏完结' : 'youxiwanjie',
+        '游戏失败' : 'youxishibai',
+        '评分' : 'pingfen',
+        '播放音乐' : 'bofangyinyue',
+        '关闭特写' : 'guanbitexie',
+        '展示特写' : 'zhanshitexie',
+        '设置房间' : 'shezhifangjian',
+        '移动中止' : 'yidongzhongzhi',
+        '设置时间' : 'shezhishijian',
+        '进入房间' : 'jinrufangjian',
+        '播放音效' : 'bofangyinxiao',
+        '设置天气' : 'shezhitianqi',
+        '停止音乐' : 'tingzhiyinyue',
+        '当数据' : 'dangshuju',
+        '设置数据' : 'shezhishuju',
+        '对比数据' : 'duibishuju',
+        '增加数据' : 'zengjiashuju',
+        '循环中止' : 'xunhuanzhongzhi',
+        '最大数据' : 'zuidashuju',
+        '最小数据' : 'zuixiaoshuju',
+        '选择' : 'xuanze',
+        '随机数据' : 'suijishuju',
+        '当时间' : 'dangshijian',
+        '复制数据' : 'fuzhishuju',
+        '数据合值' : 'shujuhezhi',
+        '数据差值' : 'shujuchazhi',
+        '定义选择' : 'dingyixuanze',
+        '添加选项' : 'tianjiaxuanxiang',
+        '去除选项' : 'quchuxuanxiang',
+        '且' : 'qie',
+        '或' : 'huo',
+        '否则' : 'fouze',
+        '那么' : 'name',
+        '如果' : 'ruguo',
+        '循环' : 'xunhuan',
+        '如果数据' : 'ruguoshuju'
+    }
 
     def run(self, edit):
         if not self._check():
             return
-        sublime.message_dialog('查看 %s %s\nTo be continued...' % (self._type, self._clob))
+        if '素材包' == self._type:
+            url = 'sucaibao/#' + self._clob
+        elif '主题' == self._type:
+            url = 'zhuti/#' + self._clob
+        elif self._clob in self._actions:
+            url = self._actions[self._clob] + '.html'
+        else:
+            url = ''
+        if url:
+            webbrowser.open_new_tab('http://dahao.de/yinqing/' + url)
 
     def _check(self):
         view = self.view

fa40077d1f4f20d8b146919e20fe3dfd3d840627
郑煜宇 <yzheng@atfacg.com>
2016-05-23 17:44:07 +0800

    增加付费解锁『选择』项地支持。

--- bigine_update.py
+++ bigine_update.py
@@ -1,7 +1,7 @@
 import sublime, sublime_plugin, webbrowser, http.client, json, re
 
 class BigineUpdateCommand(sublime_plugin.WindowCommand):
-    __version = '0.2.2'
+    __version = '0.2.3'
 
     def run(self):
         try:

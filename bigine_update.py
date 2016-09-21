2b37065189dad3d5da7dab729d0dfc162c2e1447
郑煜宇 <yzheng@atfacg.com>
2016-09-21 10:50:40 +0800

    修改当前版本信息至 0.2.7 。

--- bigine_update.py
+++ bigine_update.py
@@ -1,7 +1,7 @@
 import sublime, sublime_plugin, webbrowser, http.client, json, re
 
 class BigineUpdateCommand(sublime_plugin.WindowCommand):
-    __version = '0.2.6'
+    __version = '0.2.7'
 
     def run(self):
         try:
